437e8c567d666ada0df62c7a6caf0caf0f25bb3f
Justin Grevich <justin@grevi.ch>
2013-09-12 14:44:42 -0400

    [#56868544] set relative root in production env to '/unclaimed-money'

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,4 +61,6 @@ UnclaimedMoney::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  
+  config.action_controller.relative_url_root = '/unclaimed-money'
 end
