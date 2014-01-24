1ac779d8ba7fbe2b63642d5781842687f6cb477a
Bill Kautter <bill@mobomo.com>
2014-01-24 11:37:56 -0500

    Mods for story: Search only the three currently searched federal databases.  Because the system only searches two states currently, a minimally viable version will exclude the state functionality, https://www.pivotaltracker.com/story/show/63623370

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,14 +10,14 @@ class HomeController < ApplicationController
       @fdic_search = FdicSearch.search_for(params[:last_name])
       @pbgc_search = PbgcSearch.search_for(params[:last_name], {:page => params[:page] || 1})
       @hud_search = HudSearch.search_for(params[:last_name])    
-      @ca_search = CaUnclaimedProperty.new(params[:last_name]) if params['states'] && params['states'].include?('California')
-      @md_search = MdUnclaimedProperty.new(params[:last_name]) if params['states'] && params['states'].include?('Maryland')
+      #@ca_search = CaUnclaimedProperty.new(params[:last_name]) if params['states'] && params['states'].include?('California')
+      #@md_search = MdUnclaimedProperty.new(params[:last_name]) if params['states'] && params['states'].include?('Maryland')
     
       @has_results = true if @fdic_search[:total] > 0 or 
                              @pbgc_search[:total] > 0 or 
-                             @hud_search[:total] > 0 or 
-                             @ca_search && @ca_search.total or 
-                             @md_search && @md_search.total
+                             @hud_search[:total] > 0
+                             #@ca_search && @ca_search.total or 
+                             #@md_search && @md_search.total
     end
   end
 
