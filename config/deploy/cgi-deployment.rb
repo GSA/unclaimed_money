6167661526600abe610ea86a02e8c67662818ba1
Adam Frazin <adam@mobomo.com>
2014-02-28 14:24:34 -0500

    Fix Capistrano deployment for CGI environment

--- config/deploy/cgi-deployment.rb
+++ config/deploy/cgi-deployment.rb
@@ -11,7 +11,7 @@ set :bundle_flags, '--system --quiet'
 set :use_sudo, true
 set(:real_revision) { source.query_revision( revision ) { |cmd| capture(cmd) } }
 set :user, ENV['USER'] || 'apache'
-set :web_user, "apache"
+set :web_user, "money_user"
 set :web_group, "apache"
 set :deploy_to, "/var/www/#{application}"
 set :domain, "172.22.79.89"
@@ -27,6 +27,7 @@ set :branch, ENV['BRANCH'] || 'master'
 before 'deploy', 'deploy:take_control'
 #before 'deploy:symlink_db', 'deploy:delete_rvmrc'
 #before 'deploy:assets:precompile', 'deploy:symlink_config_for_staging'
+#before 'deploy', 'deploy:set_up_rvm'
 after 'deploy', 'deploy:relinquish_control'
 # before "deploy:update_code", "deploy:web:disable"
 # after "deploy", "deploy:web:enable"
@@ -48,5 +49,10 @@ namespace :deploy do
   task :relinquish_control do
     sudo "chown -R #{web_user}:#{web_group} #{deploy_to}/", :pty => true
     sudo "chmod -R g+rx #{deploy_to}/", :pty => true
+  end
+
+  task :set_up_rvm, :roles => :app do
+    run "cd #{release_path}; #{sudo} rvm use ruby-1.9.3-p484@unclaimed_money --create --rvmrc", :pty => true
+    sudo "chown -R #{web_user} /usr/local/rvm/gems/ruby-1.9.3-p484@unclaimed_money", :pty => true
   end
 end
