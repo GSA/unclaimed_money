f61364ad8abfc16a6fbb463fe25559e5a0318896
Adam Frazin <adam@mobomo.com>
2014-02-28 14:44:52 -0500

    Configure Capistrano for production/staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,10 +1,20 @@
+require "rvm/capistrano"
+set :rvm_ruby_string, 'ruby-1.9.3-p484@unclaimed_money'
+set :rvm_type, :system                   # Defaults to: :auto
+set :bundle_cmd, "/usr/local/rvm/gems/ruby-1.9.3-p484@global/bin/bundle"
+set :bundle_dir, ''
+set :bundle_flags, '--system --quiet'
+#set :bundle_dir, "/usr/local/rvm/gems/ruby-1.9.3-p484"
+#set :rvm_ruby_version, '1.9.3-p484'      # Defaults to: 'default'
+
 # Use this if you're stuck behind a draconian VPN
+set :use_sudo, true
 set(:real_revision) { source.query_revision( revision ) { |cmd| capture(cmd) } }
-set :user, ENV['USER'] || 'ubuntu'
-set :web_user, "mygov"
+set :user, ENV['USER'] || 'apache'
+set :web_user, "money_user"
 set :web_group, "apache"
 set :deploy_to, "/var/www/#{application}"
-set :domain, "" # TBD
+set :domain, "172.22.76.89"
 set :rails_env, :staging
 
 role :web, "#{domain}"                          # Your HTTP server, Apache/etc
@@ -15,8 +25,9 @@ set :branch, ENV['BRANCH'] || 'master'
 
 
 before 'deploy', 'deploy:take_control'
-before 'deploy:symlink_db', 'deploy:delete_rvmrc'
-before 'deploy:assets:precompile', 'deploy:symlink_config_for_staging'
+#before 'deploy:symlink_db', 'deploy:delete_rvmrc'
+#before 'deploy:assets:precompile', 'deploy:symlink_config_for_staging'
+#before 'deploy', 'deploy:set_up_rvm'
 after 'deploy', 'deploy:relinquish_control'
 # before "deploy:update_code", "deploy:web:disable"
 # after "deploy", "deploy:web:enable"
@@ -38,5 +49,10 @@ namespace :deploy do
   task :relinquish_control do
     sudo "chown -R #{web_user}:#{web_group} #{deploy_to}/", :pty => true
     sudo "chmod -R g+rx #{deploy_to}/", :pty => true
+  end
+
+  task :set_up_rvm, :roles => :app do
+    run "cd #{release_path}; #{sudo} rvm use ruby-1.9.3-p484@unclaimed_money --create --rvmrc", :pty => true
+    sudo "chown -R #{web_user} /usr/local/rvm/gems/ruby-1.9.3-p484@unclaimed_money", :pty => true
   end
 end
